5f8a79aabd789825e4f8f137efaf1f0b74a705b8
Andrey Oskin <skoffer@arkoniak.com>
2020-04-20 21:58:44 +0300

    preliminary compress utilities

--- src/UrlDownload.jl
+++ src/UrlDownload.jl
@@ -18,31 +18,39 @@ const ext2sym = Dict(
 )
 
 const sym2func = Dict(
-    :FEATHER => (x, y; kw...) -> load_feather(x, y; kw...),
-    :PIC => (x, y; kw...) -> load_pic(x, y; kw...),
-    :CSV => (x, y; kw...) -> load_csv(x, y; kw...),
-    :TSV => (x, y; kw...) -> load_csv(x, y; kw...),
-    :JSON => (x, y; kw...) -> load_json(x, y; kw...)
+    :FEATHER => (x; kw...) -> load_feather(x; kw...),
+    :PIC => (x; kw...) -> load_pic(x; kw...),
+    :CSV => (x; kw...) -> load_csv(x; kw...),
+    :TSV => (x; kw...) -> load_csv(x; kw...),
+    :JSON => (x; kw...) -> load_json(x; kw...)
 )
 
-function load_feather(buf, data; kw...)
+const Compressor = Dict(
+    :gzip => (lib = :CodecZlib, stream = :GzipDecompressorStream),
+    :zstd => (lib = :CodecZstd, stream = :ZstdDecompressorStream),
+    :xz => (lib = :CodecXz, stream = :XzDecompressorStream),
+    :lz4 => (lib = :CodecLz4, stream = :LZ4FrameDecompressorStream),
+    :bzip2 => (lib = :CodecBzip2, stream = :Bzip2DecompressorStream)
+)
+
+function load_feather(buf; kw...)
     lib = checked_import(:Feather)
     return Base.invokelatest(lib.read, buf)
 end
 
-function load_csv(buf, data; kw...)
+function load_csv(buf; kw...)
     lib = checked_import(:CSV)
     return Base.invokelatest(lib.File, buf; kw...)
 end
 
-function load_pic(buf, data; kw...)
+function load_pic(buf; kw...)
     lib = checked_import(:ImageMagick)
-    return Base.invokelatest(lib.load_, data)
+    return Base.invokelatest(lib.load_, _getdata(buf))
 end
 
-function load_json(buf, data; kw...)
+function load_json(buf; kw...)
     lib = checked_import(:JSON3)
-    return Base.invokelatest(lib.read, data)
+    return Base.invokelatest(lib.read, _getdata(buf))
 end
 
 # Borrowed directly from FileIO
@@ -85,13 +93,63 @@ function datatype(url)
     error("$ext is unsupported.")
 end
 
-function wrapdata(url, data, format; kw...)
-    buf = IOBuffer(data)
-    dtype = format == nothing ? datatype(url) : format
+# this set of functions is needed for distinguish between raw data and zipstreams,
+# we do not want to wrap/unwrap zipstreams.
+createbuffer(data::Vector{UInt8}) = IOBuffer(data)
+createbuffer(data) = data
 
-    sym2func[dtype](buf, data; kw...)
+_getdata(buf::IOBuffer) = buf.data
+_getdata(buf) = buf
+
+function wrapdata(url, data, format, parser, error_on_undetected_format = true; kw...)
+    if isnothing(parser)
+        buf = createbuffer(data)
+        dtype = format == nothing ? datatype(url) : format
+
+        if dtype in keys(sym2func)
+            return sym2func[dtype](buf; kw...)
+        else
+            if error_on_undetected_format
+                @error "Data format $dtype is not supported"
+            else
+                @warn "Data format $dtype is not supported"
+                return data
+            end
+        end
+    else
+        return parser(data; kw...)
+    end
 end
 
+# Check the file format of a stream.
+# Taken from TableReader.jl
+function checkformat(data, url)
+    if length(data) >= 6
+        magic = data[1:6]
+    else
+        error("Incomplete data received")
+    end
+    if magic[1:6] == b"\xFD\x37\x7A\x58\x5A\x00"
+        return :xz
+    elseif magic[1:2] == b"\x1f\x8b"
+        return :gzip
+    elseif magic[1:4] == b"\x28\xb5\x2f\xfd"
+        return :zstd
+    elseif magic[1:3] == b"\x42\x5A\x68"
+        return :bzip2
+    elseif magic[1:4] == b"\x04\x22\x4D\x18"
+        return :lz4
+    end
+
+    # it's too ambigous to detect zip with magic bytes
+    _, ext = splitext(url)
+    if (ext == ".zip") | (ext == ".z")
+        return :zip
+    end
+
+    # we are giving up
+    return :none
+end
 
 """
     urldownload(url, progress = false; parser = nothing, format = nothing, headers = HTTP.Header[], httpkw = Pair[], update_period = 1, kw...)
@@ -111,7 +169,10 @@ pairs.
 * `update_period`: period of `ProgressMeter` update, by default 1 sec
 * `kw...`: any keyword arguments that should be passed to the data parser.
 """
-function urldownload(url, progress = false; parser = nothing, format = nothing, headers = HTTP.Header[],
+function urldownload(url, progress = false;
+        parser = nothing, format = nothing,
+        compress = :auto, multifiles = false,
+        headers = HTTP.Header[],
         update_period = 1, httpkw = Pair[], kw...)
     body = UInt8[]
     HTTP.open("GET", url, headers; httpkw...) do stream
@@ -130,10 +191,40 @@ function urldownload(url, progress = false; parser = nothing, format = nothing, 
         end
     end
 
-    if parser == nothing
-        return wrapdata(url, body, format; kw...)
+    compress = compress == :auto ? checkformat(body, url) : compress
+
+    if compress == :none
+        # skip unzipping entirely, it's parser responisbility to process the data
+        wrapdata(url, body, format, parser; kw...)
+    elseif compress == :zip
+        zlib = checked_import(:ZipFile)
+        zread = getfield(zlib, :Reader)(IOBuffer(body)).files
+        if multifiles
+            return [wrapdata(z.name, z, format, parser, false; kw...) for z in zread]
+        else
+            if length(zread) > 1
+                @warn "More than one file in zip archive, returning first."
+            elseif length(zread) == 0
+                @error "Zip archive is empty."
+            end
+            zread = zread[1]
+            # This one can easily fail for non csv files, nothing I can do about it
+            wrapdata(zread.name, zread, format, parser; kw...)
+        end
+    elseif compress in keys(Compressor)
+        # it's one of the TranscodingStreams.jl streams, not much to do here,
+        # defaults to CSV/custom parser
+        if isnothing(parser)
+            lib = checked_import(Compressor[compress].lib)
+            stream = getfield(lib, Compressor[compress].stream)
+            csvlib = checked_import(:CSV)
+            csvlibfile = getfield(csvlib, :File)
+            return Base.invokelatest(csvlibfile, Base.invokelatest(stream, IOBuffer(body)); kw...)
+        else
+            return parser(body; kw...)
+        end
     else
-        return parser(body; kw...)
+        error("Unknown compress format: $compress")
     end
 end
 
---
+++ test/test03_compress.jl
@@ -0,0 +1,53 @@
+module TestCompress
+using Test
+using UrlDownload
+
+@testset "compress" begin
+    url = "https://raw.githubusercontent.com/Arkoniak/UrlDownload.jl/master/data/test.xz"
+    urldownload(url)
+    urldownload(url, compress = :xz)
+
+    url = "https://raw.githubusercontent.com/Arkoniak/UrlDownload.jl/master/data/test.gz"
+    urldownload(url)
+    urldownload(url, compress = :gzip)
+
+    url = "https://raw.githubusercontent.com/Arkoniak/UrlDownload.jl/master/data/test.bz2"
+    urldownload(url)
+    urldownload(url, compress = :bzip2)
+
+    url = "https://raw.githubusercontent.com/Arkoniak/UrlDownload.jl/master/data/test.lz4"
+    urldownload(url)
+    urldownload(url, compress = :lz4)
+
+    url = "https://raw.githubusercontent.com/Arkoniak/UrlDownload.jl/master/data/test.zst"
+    urldownload(url)
+    urldownload(url, compress = :zstd)
+end
+
+@testset "zip compress" begin
+    url = "https://raw.githubusercontent.com/Arkoniak/UrlDownload.jl/master/data/ext.csv.zip"
+    urldownload(url)
+
+    url = "https://raw.githubusercontent.com/Arkoniak/UrlDownload.jl/master/data/test2.zip"
+    urldownload(url)
+
+    url = "https://raw.githubusercontent.com/Arkoniak/UrlDownload.jl/master/data/test2.zip"
+    urldownload(url, multifile = true)
+end
+
+@testset "compress overrides" begin
+    url = "https://raw.githubusercontent.com/Arkoniak/UrlDownload.jl/master/data/test.zst"
+    urldownload(url, parser = identity)
+
+    url = "https://raw.githubusercontent.com/Arkoniak/UrlDownload.jl/master/data/test.zst"
+    urldownload(url, compress = :none, parser = identity)
+
+    url = "https://raw.githubusercontent.com/Arkoniak/UrlDownload.jl/master/data/test2.zip"
+    urldownload(url, parser = identity)
+
+    url = "https://raw.githubusercontent.com/Arkoniak/UrlDownload.jl/master/data/test2.zip"
+    urldownload(url, compress = :none, parser = identity)
+end
+
+
+end # module
